4f9ef36feb7e6392c1a7803078b53f71e07a7654
Fellipe Emanoel <fellipe.silva97@hotmail.com>
2021-01-18 15:45:32 -0300

    Ajustando a validação de formulário

--- appgobarber/src/pages/SignUp/index.tsx
+++ appgobarber/src/pages/SignUp/index.tsx
@@ -1,27 +1,70 @@
 import React, { useCallback, useRef } from "react";
-import {Image, View, ScrollView, KeyboardAvoidingView, Platform, TextInput} from "react-native";
+import {Image, View, ScrollView, KeyboardAvoidingView, Platform, TextInput, Alert} from "react-native";
 import Icon from 'react-native-vector-icons/Feather';
 import {useNavigation} from "@react-navigation/native";
 import {Form} from '@unform/mobile';
 import {FormHandles, useField} from "@unform/core"
+import * as Yup from "yup";
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
+
+import getValidationErrors from '../../utils/getValidationErros';
 
 import logoImg from '../../assets/logo.png';
 
 import {Container, Title, BackToSignInButton, BackToSignInText} from "./styles";
 
+interface SignUpFormData{
+    name: string;
+    email:string;
+    password: string;
+}
+
 const SignUp: React.FC = () => {
 
     const navigation = useNavigation();
     const formRef = useRef<FormHandles>(null);
-    const emailInputRef = useRef<TextInput>(null);
-    const passwordInputRef = useRef<TextInput>(null);
+    const emailInputRef = useRef<TextInput>(null);    const passwordInputRef = useRef<TextInput>(null);
+   
 
-    const handleSignUp = useCallback((data:object) =>{
-        console.log(data);
-    }, []);
+    const handleSignUp = useCallback(
+        async (data: SignUpFormData) => {
+          try {
+            formRef.current?.setErrors({});
+    
+            const schema = Yup.object().shape({
+              name: Yup.string().required('O nome é obrigatório'),
+              email: Yup.string()
+                .email('Digite um e-mail válido')
+                .required('E-mail obrigatório'),
+              password: Yup.string().min(6, 'No mínimo 6 digitos.'),
+            });
+    
+            await schema.validate(data, {
+              abortEarly: false,
+            });
+    
+            // await signIn({
+            //   email: data.email,
+            //   password: data.password,
+            // });
+    
+            //history.push('/dashboard');
+          } catch (err) {
+            if (err instanceof Yup.ValidationError) {
+              const errors = getValidationErrors(err);
+    
+              formRef.current?.setErrors(errors);
+    
+              return;
+            }
+    
+            Alert.alert('Erro ao realizar cadastro', 'Ocorreu um erro ao realizar cadastro, tente novamente');
+            
+          }
+        },
+        []);
 
     return (
         <>
